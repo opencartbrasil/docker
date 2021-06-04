3f2305881907c016d8d092188852e4f47f9aa678
Valdeir S <valdeir@v5market.com>
2021-06-04 16:21:25 -0300

    feat: adiciona validação antes da instalação via CLI
    Agora, verifica se todos os dados do banco de dados e servidor estão
    prenchidos, antes de realizar a instalação automática.
    
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- latest/php7.4/fpm/docker-entrypoint.sh
+++ latest/php7.4/fpm/docker-entrypoint.sh
@@ -43,28 +43,60 @@ if [[ "$command" =~ start|php-fpm|apache2* ]]; then
 
         timeout=0;
         db_available=0
-        while [ $timeout -lt 120 ]; do
-            nc -z -w1 $OCBR_DB_HOST $OCBR_DB_PORT
-            if [ $? -eq 0 ]; then
-                db_available=1
-                break;
+        db_data=1
+        declare -a db_config=(
+            "${OCBR_DB_DATABASE:-0}"
+            "${OCBR_DB_DRIVER:-0}"
+            "${OCBR_DB_HOST:-0}"
+            "${OCBR_DB_PASS:-0}"
+            "${OCBR_DB_PORT:-0}"
+            "${OCBR_DB_PREFIX:-0}"
+            "${OCBR_DB_USER:-0}"
+            "${OCBR_HTTP_SERVER:-0}"
+        )
+
+        for db_config in ${db_config[@]}; do
+            if [ $db_config = "0" ]; then
+                db_data=0
             fi
-            timeout=$(( $timeout + 1 ));
-            echo "Aguardando conexão com banco de dados. Tentativa $timeout/120"
-            sleep 1
         done
 
-        if [ $db_available -eq 1 ]; then
-            echo ""
-            echo "Configurando dados"
-            php /assets/configure-oc.php
-            cp /assets/admin-config-docker.php admin/config.php
-            cp /assets/config-docker.php config.php
-            chown "${user}:${group}" admin/config.php config.php
-        else
-            echo "Não foi possível conectar-se ao banco de dados"
+        if [ $db_data -eq 1 ]; then
+            while [ $timeout -lt 120 ]; do
+                nc -z -w1 $OCBR_DB_HOST $OCBR_DB_PORT
+                if [ $? -eq 0 ]; then
+                    db_available=1
+                    break;
+                fi
+                timeout=$(( $timeout + 1 ));
+                echo "Aguardando conexão com banco de dados. Tentativa $timeout/120"
+                sleep 1
+            done
+
+            if [ $db_available -eq 1 ]; then
+                echo ""
+                echo "Configurando dados"
+                php /assets/configure-oc.php
+                cp /assets/admin-config-docker.php admin/config.php
+                cp /assets/config-docker.php config.php
+                chown "${user}:${group}" admin/config.php config.php
+            else
+                echo "Não foi possível conectar-se ao banco de dados"
+            fi
         fi
     fi
 fi
 
-exec "$@"
+if [ $command = "start" ]; then
+    for oc_file in \
+        "/var/www/html/admin/config.php" \
+        "/var/www/html/config.php";
+    do
+        [[ ! -f $oc_file ]] && touch $oc_file;
+        chown www-data:www-data $oc_file;
+    done;
+
+    php -S 0.0.0.0:8888 -t /var/www/html
+else
+    exec "$@"
+fi
